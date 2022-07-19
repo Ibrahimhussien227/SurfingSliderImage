5c7cca269468d4e3898b4f50628977329f3c1e56
Ibrahimhussien227 <ibrahimhussien227@gmail.com>
2022-07-19 14:51:09 +0300

    ready to deploy

--- src/components/pages/Main/sliderImages/SliderData.js
+++ src/components/pages/Main/sliderImages/SliderData.js
@@ -33,7 +33,7 @@ export const settings = {
       breakpoint: 1024,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
         infinite: true,
         dots: true,
       },
@@ -42,14 +42,14 @@ export const settings = {
       breakpoint: 600,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
         initialSlide: 2,
       },
     },
     {
       breakpoint: 480,
       settings: {
-        slidesToShow: 1,
+        slidesToShow: 2,
         slidesToScroll: 1,
       },
     },
